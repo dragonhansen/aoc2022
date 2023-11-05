91278fb14ce17c00cbd9c66e154fbf45cc18f6c0
Oliver <oliv5023@gmail.com>
2023-11-05 18:59:32 +0100

    day 6 finished

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
 ﻿using Days;
-
+/*
 Day1.Main();
 Day2.Main();
 Day3.Main();
 Day4.Main();
 Day5.Main();
+*/
+Day6.Main();
---
+++ days/Day6.cs
@@ -0,0 +1,25 @@
+using System.Threading.Tasks.Dataflow;
+
+namespace Days {
+    static class Day6 {
+        static string input = File.ReadAllText("inputs/input_6.txt");
+
+        private static int findMarker(int markerLength) {
+            string currentFour = input.Substring(0, markerLength);
+            for (int i = markerLength-1; i<input.Length; i++) {
+                var groups = currentFour.GroupBy(c => c);
+                if (groups.Any(g => g.Count() > 1)) {
+                    currentFour = input.Substring(i, markerLength);
+                    continue;
+                }
+                return i + markerLength -1;                
+            }
+            return -1;
+        }
+
+        public static void Main() {
+                Console.WriteLine($"Part one: {findMarker(4)}");
+                Console.WriteLine($"Part one: {findMarker(14)}");   
+        }
+    }
+}
